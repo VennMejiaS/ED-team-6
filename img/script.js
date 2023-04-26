89a66572603c1385baf0f67b73b558db4545ace1
VennMejiaS <venus.mejiap@gmail.com>
2023-04-26 18:07:35 -0500

    second commit

---
+++ img/script.js
@@ -0,0 +1,13 @@
+const type = document.getElementById("e-mail");
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+})
+const isValid = true; 
+if (isValid){
+window.location.href = "vista estudiante.html";
+  }
+
+const isValid = true; 
+if (isValid){
+window.location.href = "vista docente.html";
+  }
