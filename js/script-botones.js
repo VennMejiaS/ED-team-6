c9248aa072f728e67c673b1abaeca1afca82bfb8
VennMejiaS <venus.mejiap@gmail.com>
2023-05-30 17:36:09 -0500

    mvp

--- js/script-botones.js
+++ js/script-botones.js
@@ -425,3 +425,195 @@ const mapaDeValoresCre = {
     "perspectiva",
     "cr12"
   );
+
+  function cargarVideo(videoId) {
+    var videoContainer = document.getElementById('video-container');
+    videoContainer.innerHTML = '<iframe width="429" height="270" src="https://www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>';
+  }
+
+//Total de medallas
+const mapaDeValoresTot = {
+  totalMedallas: {
+    conocimiento: null,
+    metodo: null,
+    activa: null,
+    expresion: null,
+    inquietudes: null,
+    compromisos: null,
+    visuales: null,
+    perspectiva: null,
+  },
+};
+const mapaDeMedallasTot = {
+  plataTot: 0,
+  bronceTot: 1,
+  oroTot: 2,
+};
+
+function obtenerNombreDeMedallaTot(numeroDeMedallaTot) {
+  if (numeroDeMedallaTot === 0) {
+    return "plata";
+  } else if (numeroDeMedallaTot === 1) {
+    return "bronce";
+  } else if (numeroDeMedallaTot === 2) {
+    return "oro";
+  }
+  return "medalla no es valida";
+}
+
+function actualizarResultadosTot(
+  areaDeEvaluacionTot,
+  idDelContenedorDeLosResultadosTot
+) {
+  const elementoDeResultadosTot = document.getElementById(
+    idDelContenedorDeLosResultadosTot
+  );
+  const contenedoresDeResultadosTot =
+    elementoDeResultadosTot.getElementsByTagName("span");
+
+  // elementos html en javascript
+  const resultadoPlataDom = contenedoresDeResultadosTot[0];
+  const resultadoBronceDom = contenedoresDeResultadosTot[1];
+  const resultadoOroDom = contenedoresDeResultadosTot[2];
+
+  // el area de evaluacion en el mapa de valores
+  const resultadosDelAreaTot = mapaDeValoresTot[areaDeEvaluacionTot];
+
+  // medallas obtenidas
+  const resultadoConocimientoTot = resultadosDelAreaTot.conocimiento;
+  const resultadoMetodoTot = resultadosDelAreaTot.metodo;
+  const resultadoActivaTot = resultadosDelAreaTot.activa;
+  const resultadoExpresionTot = resultadosDelAreaTot.expresion;
+  const resultadoInquietudesTot = resultadosDelAreaTot.inquietudes;
+  const resultadoCompromisosTot = resultadosDelAreaTot.compromisos; 
+  const resultadoVisualesTot = resultadosDelAreaTot.visuales;
+  const resultadoPerspectivaTot = resultadosDelAreaTot.perspectiva;
+
+  const bolsaDeMedallasTot = [resultadoConocimientoTot, resultadoMetodoTot, resultadoActivaTot, resultadoExpresionTot, resultadoInquietudesTot, resultadoCompromisosTot, resultadoVisualesTot, resultadoPerspectivaTot];
+
+  let totalDePlatas = 0,
+    totalDeBronces = 0,
+    totalDeOros = 0;
+
+  // obtenemos el valor de cada medalla
+  for (const medalla of bolsaDeMedallasTot) {
+    if (medalla === "plata") {
+      totalDePlatas++;
+    } else if (medalla === "bronce") {
+      totalDeBronces++;
+    } else if (medalla === "oro") {
+      totalDeOros++;
+    }
+
+    resultadoPlataDom.innerHTML = totalDePlatas;
+    resultadoBronceDom.innerHTML = totalDeBronces;
+    resultadoOroDom.innerHTML = totalDeOros;
+  }
+
+  console.log(elementosDeLosResultadosTot);
+}
+
+function anadirEventosARadiosTot(
+  nombreDeLosBotones,
+  areaDeEvaluacion,
+  topicoAEvaluar,
+  idDelContenedorDeLosResultados
+) {
+  const botonesTotalTarjetas =
+    document.getElementsByName(nombreDeLosBotones);
+  for (
+    let medallaIndex = 0;
+    medallaIndex < botonesTotalTarjetas.length;
+    medallaIndex++
+  ) {
+    const radioButton = botonesTotalTarjetas[medallaIndex];
+
+      radioButton.addEventListener("change", function () {
+      const NombreDeMedallaTot = obtenerNombreDeMedallaTot(medallaIndex);
+      mapaDeValoresTot[areaDeEvaluacion][topicoAEvaluar] = NombreDeMedallaTot;
+      actualizarResultadosTot(areaDeEvaluacion, idDelContenedorDeLosResultados);
+    });
+  }
+}
+
+anadirEventosARadiosTot(
+  "pensamiento_critico_conocimiento",
+  "totalMedallas",
+  "conocimiento",
+  "tct"
+);
+
+anadirEventosARadiosTot(
+  "pensamiento_critico_metodo",
+  "totalMedallas",
+  "metodo",
+  "tct"
+);
+
+anadirEventosARadiosTot(
+  "comunicacion_activa",
+  "totalMedallas",
+  "activa",
+  "tct"
+);
+
+anadirEventosARadiosTot(
+  "comunicacion_expresion",
+  "totalMedallas",
+  "expresion",
+  "tct"
+);
+
+anadirEventosARadiosTot(
+  "trabajo_colaborativo_inquietudes",
+  "totalMedallas",
+  "inquietudes",
+  "tct"
+);
+
+anadirEventosARadiosTot(
+  "trabajo_colaborativo_compromisos",
+  "totalMedallas",
+  "compromisos",
+  "tct"
+);
+anadirEventosARadiosTot(
+  "creatividad_visuales",
+  "totalMedallas",
+  "visuales",
+  "tct"
+);
+
+anadirEventosARadiosTot(
+  "creatividad_perspectiva",
+  "totalMedallas",
+  "perspectiva",
+  "tct"
+);
+
+// Obtén una referencia al botón de guardar
+var guardarBoton = document.getElementById('Save');
+
+// Agrega un evento de clic al botón de guardar
+guardarBoton.addEventListener('click', function() {
+  // Obtén los valores de los <span>
+  var totalPlata = document.getElementById('total-plata').textContent;
+  var totalBronce = document.getElementById('total-bronce').textContent;
+  var totalOro = document.getElementById('total-oro').textContent;
+
+  // Almacena los datos en el localStorage
+  localStorage.setItem('total-plata', totalPlata);
+  localStorage.setItem('total-bronce', totalBronce);
+  localStorage.setItem('total-oro', totalOro);
+
+  // Notifica al usuario que los datos han sido guardados
+  alert('La evaluación ha sido guardada con éxito');
+});
+
+var totalPlata = localStorage.getItem('total-plata');
+var totalBronce = localStorage.getItem('total-bronce');
+var totalOro = localStorage.getItem('total-oro');
+
+document.getElementById('obtenidas-plata').textContent = totalPlata;
+document.getElementById('obtenidas-bronce').textContent = totalBronce;
+document.getElementById('obtenidas-oro').textContent = totalOro;
--- js/script.js
+++ js/script.js
@@ -1,12 +1,42 @@
-const boton = document.getElementById("button")
-boton addEventListener("click")
-  function validarol(){
-   if(document.getElementById('estudiante') && document.getElementById('estudiante').value == variableemail){
-    window alert('E-mail o rol incorrecto)')
-    location.replace('estudiante.html')
-  }
-  else{
-    location.replace('docente.html')
-  }
+const boton = document.getElementById("button");
+boton.addEventListener("click", ingresar);
+
+function ingresar() {
+  const email = document.getElementById("e-mail").value;
+  const url = `https://team-6-back.onrender.com/docs/#/User/get_api_users`; 
   
+  fetch('https://team-6-back.onrender.com/docs/#/User/get_api_users', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      const estudianteEmail = data.estudianteEmail; 
+      const docenteEmail = data.docenteEmail; 
+      const rol = data.rol; 
+
+      console.log(email, estudianteEmail, docenteEmail, rol); 
+
+      if (email === estudianteEmail && rol === 'estudiante') {
+        // Redirigir a estudiante.html si el correo y el rol son correctos para un estudiante
+        location.replace('estudiante.html');
+      } else if (email === docenteEmail && rol === 'docente') {
+        // Redirigir a docente.html si el correo y el rol son correctos para un docente
+        location.replace('docente.html');
+      } else {
+        // Mostrar alerta si el correo o el rol son incorrectos
+        alert('E-mail o rol incorrecto');
+      }
+    })
 }
+
+// Obtén una referencia al botón "Ingresar"
+const botonIngresar = document.getElementById('btn-ingresar');
+
+// Agrega un evento de clic al botón
+botonIngresar.addEventListener('click', function() {
+  // Redirige a la página "estudiante.html"
+  window.location.href = '../estudiante.html';
+});
